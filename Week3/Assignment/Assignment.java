17e4c2a1fc66fc4e2eb1dd338b15f4e76ea7b104
kyuyeonrhee <43326260+kyuyeonrhee@users.noreply.github.com>
2020-03-01 00:26:22 +0900

    20200301_java
    20200301_java

--- Week3/Assignment/Assignment.java
+++ Week3/Assignment/Assignment.java
@@ -5,21 +5,20 @@ import org.apache.commons.csv.*;
 public class Assignment {
     public void countryInfo (CSVParser parser, String country) {
         for (CSVRecord record: parser) {
-            String name = record.get("Country");
-            
-            if (name.contains(country)) {
+            if (record.get("Country").equals(country)) {
                 String export = record.get("Exports");
-                String value = record.get("Value (dollars)");
+                String value = record.get("Value (dollars)");// do something
                 
                 if ((export.length() > 0) && (value.length() > 0)){
-                    System.out.println(country + ": " + export + ": " + value);   
-                }
+                    System.out.println(country + ": " + export + ": " + value);
+                }   
             }
             else {
                 System.out.println("NOT FOUND");
             }
         }
     }
+
     
     public void listExportersTwoProducts (CSVParser parser, String exportItem1, String exportItem2){
         for (CSVRecord record: parser) {
@@ -36,21 +35,21 @@ public class Assignment {
         int n = 0;
         for (CSVRecord record: parser) {
             String export = record.get("Exports");
+            
             if (export.contains(exportItem)) {
                 n++;
-                }              
+            }
         }
-            return n;
-        }
+        return n;
+    }
     
     public void bigExporters (CSVParser parser, String amount) {
         for (CSVRecord record: parser) {
-            String dollar = record.get("Value (dollars)");
-            
-            if (dollar.length() > amount.length()) {
+            if (record.get("Value (dollars)").length() > amount.length()) {
                 String country = record.get("Country");
+                String dollar = record.get("Value (dollars)");
                 System.out.println(country + " " + dollar);
-                }
+            }
             }
         }
         
@@ -58,10 +57,10 @@ public class Assignment {
     public void tester() {
         FileResource fr = new FileResource();
         CSVParser parser = fr.getCSVParser();
-        countryInfo(parser, "Germany");
-        listExportersTwoProducts(parser, "gold", "diamonds");
-        numberOfExporters(parser, "gold");
-        bigExporters(parser, "$40,000,000");
+        //countryInfo(parser, "Nauru");
+        //listExportersTwoProducts(parser, "fish", "nuts");
+        //numberOfExporters(parser, "gold");
+        bigExporters(parser, "$999,999,999,998");
     }
     
 
